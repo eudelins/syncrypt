76f2a556b5dca33e5ffad733d332b24d8ff5440b
eudelins <eudelins>
2024-06-02 19:11:06 +0200

    Tool update

--- README.md
+++ README.md
@@ -1,27 +1,73 @@
-# Sync
+# Syncrypt
 
-## Setup
+## System dependencies
 
-### System dependencies
+`7z`, `git` and `python3` must be installed on your machine (only tested with Python 3.11.9!).
 
-`7z`, `git` and `python3` must be installed on your machine.
-
-### Python dependencies
-
-```bash
-python3 -m venv venv
-source ./venv/bin/activate
-pip3 -r requirements.txt
-```
 
 ## Init
 
+First, you must create a private repository on GitHub (or other plateform alike):
+
+![](screenshots/Init_GitHub.png)
+
+
+Then, you must create an empty directory on your local machine run the traditional commands to init the repository:
+
 ```bash
-python3 sync.py --init
+cd $PROJECT_FOLDER
+git init
+git branch -M main
+git remote add $PRIVATE_REPO_URL
 ```
 
-## Sync
+Import this Python script at the root of this folder, with your Obsidian vaults to sync:
+
+```
+private_git_project_folder/
+├─ .git/
+├─ sync.py
+├─ Obsidian_Vault1/
+│  ├─ .obsidian/
+│  ├─ Note.md
+│  ├─ ...
+├─ Obsidian_Vault2/
+├─ ...
+```
+
+Then, use the following command to init the password for the encrypted archives that will be created:
 
 ```bash
-python3 sync.py --sync
+python3 sync.py init
+```
+
+Of course, the password is created in a folder that is part of the `.gitignore` so it is not pushed to the remote repo and is only stored locally.
+
+Here is a screenshot that summarize these steps:
+
+![](screenshots/Init_Syncrypt.png)
+
+## Push your changes
+
+Once the init step is done, you can push your changes with the following command:
+
+```bash
+python3 sync.py push
+```
+
+## Pull your changes
+
+First, you need to clone the directory from your GitHub and import the password use for the encryption inside the cloned folder:
+
+```bash
+git clone $PRIVATE_REPO_URL
+cd $FOLDER_NAME
+mkdir .pwd
+echo $PASSWORD > .pwd/7z_password
+```
+
+Then, you can use the pull command of the tool:
+
+```bash
+python3 sync.py pull
 ```
--- sync.py
+++ sync.py
@@ -10,6 +10,7 @@ import subprocess
 
 
 BOLD_BLUE = "\033[1;34m"
+BOLD_GREEN = "\033[1;33m"
 BOLD_RED = "\033[1;31m"
 RESET_SEQ = "\033[0m"
 
@@ -23,7 +24,10 @@ def log_error(*args):
     """Displays an error logging message"""
     print(f"{BOLD_RED}[-]{RESET_SEQ}", *args)
 
-# TODO: Add log_success (use it after init, sync success)
+
+def log_success(*args):
+    """Displays a success logging message"""
+    print(f"{BOLD_GREEN}[+]{RESET_SEQ}", *args)
 
 
 def parse_args():
@@ -35,26 +39,40 @@ def parse_args():
 
     init_subparser = subparsers.add_parser(
         "init",
-        help="Creates the 7z encryption password and prepare the git configuration to sync your notes.",
+        help=(
+            "Creates the 7z encryption password and prepare the git "
+            "configuration to sync your notes."
+        ),
     )
     init_subparser.set_defaults(exec_command=init)
 
-    sync_subparser = subparsers.add_parser(
-        "sync",
-        help="Sync doc.",
+    push_subparser = subparsers.add_parser(
+        "pull",
+        help="Pull latest changes from your remote repository.",
     )
-    sync_subparser.set_defaults(exec_command=sync)
+    push_subparser.set_defaults(exec_command=pull)
+
+    push_subparser = subparsers.add_parser(
+        "push",
+        help="Push current changes to your remote repository.",
+    )
+    push_subparser.set_defaults(exec_command=push)
     return parser.parse_args()
 
 
+BACKUP_BRANCH = "backup_branch"
 BACKUP_FOLDER = ".backup"
+BACKUP_REMOTE_NAME = "backup_dir"
 PWD_SIZE = 32
 PWD_DIR = ".pwd"
 PWD_FILE = f"{PWD_DIR}/7z_password"
 
 
 def init():
-    """Creates the 7z password for the project and prepares the git configuration"""
+    """
+    Creates the 7z password for the project and prepares the git
+    configuration.
+    """
     log_info(f"Creating '{PWD_DIR}' folder and '{PWD_FILE}' file...")
     if os.path.exists(PWD_DIR):
         log_error(f"The '{PWD_DIR}' folder already exists.")
@@ -65,16 +83,25 @@ def init():
         exit(1)
     else:
         os.makedirs(PWD_DIR)
-        log_info(f"'{PWD_DIR}' folder successfully created!")
         with open(PWD_FILE, "w") as password_file:
             password = secrets.token_hex(PWD_SIZE)
             password_file.write(password)
-            log_info(f"'{PWD_FILE}' successfully created!")
-            # TODO: git init in each directory
+            log_success(f"'{PWD_DIR}' and '{PWD_FILE}' successfully created!")
+
+    if not os.path.exists(".gitignore"):
+        log_info("Creating .gitignore...")
+        with open(".gitignore", "w") as gitignore_file:
+            gitignore_file.write("# Projects folders\n")
+            gitignore_file.write(".backup\n")
+            gitignore_file.write(".pwd\n")
+            gitignore_file.write("venv\n\n")
+            gitignore_file.write("# Obsidian notes folders\n")
+            log_success(".gitignore successfully created!")
 
 
-def retrieve_password():
-    """Tries to retrieve the 7z encryption password"""
+def retrieve_password() -> str:
+    """Tries to retrieve the 7z encryption password."""
+    log_info("Trying to retrieve encryption password...")
     if not os.path.exists(PWD_FILE):
         log_error(f"'{PWD_FILE}' could not been found!\n")
         log_error(
@@ -83,61 +110,149 @@ def retrieve_password():
         )
         exit(2)
     with open(PWD_FILE, "r") as password_file:
-        return password_file.read()
+        password = password_file.read()
+        log_success("Password successfully retrieved!")
+        return password
 
 
-def sync():
-    log_info("Fetching the remote repository changes...")
-    log_info("git fetch")
-    os.system("git fetch")
-    log_info("Checking if changes have been added...")
-    log_info("git diff --name-only origin/main")
-    subprocess.getoutput()
-    return 0
+def run_command(command: str, ignore_error: bool = False) -> int:
+    """Executes and logs a shell command."""
+    log_info(command)
+    rc = os.system(command)
+    if rc != 0:
+        if ignore_error:
+            log_error(f"{command} failed.")
+        else:
+            log_error(f"{command} failed. Aborting.")
+            exit(rc)
+    else:
+        log_success("Success!")
+    return rc
 
-    # # Pull operation
-    # Stash/commit current state?
-    # backup_notes() 
-    # git pull
-    # for each encrypted_file
-        # 7z x -p{pwd} encrypted_file
-        # git remote add backup_dir ../.backup/encrypted_file_name 
-        # git pull backup_dir main:backup_branch
 
-    # log_info("7z encryption password successfully retrieved")
+def pull():
+    """Pulls the latest encrypted notes from GitHub, and decrypts them."""
+    backup_notes()
 
-    # if git status
-    # # Push operation
-    # backup_notes()
-    # retrieve pwd
-    # for each notes_folder
-        # cd notes_folder && git commit -m "Sync" && cd -
-        # files_and_dirs = ls -a notes_folder
-        # rm -r notes_folder/files_and_dirs
-        # 7z a -p{pwd} notes_folder_git.7z notes_folder/.git
-    # git push
+    log_info("Pulling latest changes...")
+    for dir in filter(contains_git_dir, get_obsidian_folders()):
+        shutil.rmtree(dir)
+    run_command("git pull")
+
+    password = retrieve_password()
+    encrypted_archives = filter(lambda file: file.endswith(".7z"), listdir())
+    for enc_archive in encrypted_archives:
+        log_info(f"Decrypting {enc_archive}...")
+        run_command(f"7z x -y -p{password} {enc_archive}")
+
+        log_info("Restoring notes...")
+        uncompressed_archive = enc_archive.removesuffix(".7z")
+        run_command(f"cd {uncompressed_archive} && git restore .")
+
+    log_info("Merging with backup notes...")
+    for dir in filter(contains_git_dir, get_obsidian_folders()):
+        backup_dir = f"{BACKUP_FOLDER}/{dir}"
+        if contains_git_dir(backup_dir):
+            run_command(
+                f"cd {dir} && "
+                f"git remote add {BACKUP_REMOTE_NAME} ../{backup_dir}",
+                ignore_error=True,
+            )
+            run_command(
+                f"cd {dir} && git fetch {BACKUP_REMOTE_NAME}",
+                ignore_error=True,
+            )
+            rc = run_command(
+                f"cd {dir} && git merge {BACKUP_REMOTE_NAME}/{BACKUP_BRANCH}",
+                ignore_error=True,
+            )
+            if rc != 0:
+                log_error(
+                    "Merge failed. Carefully check the logs and resolve your "
+                    "conflicts."
+                )
+
+
+def push():
+    """Encrypts the notes and push them on GitHub."""
+    backup_notes()
+    password = retrieve_password()
+    for dir in get_obsidian_folders():
+        if ".git" not in listdir(dir):
+            add_to_gitignore(dir)
+            run_command(f"cd {dir} && git init")
+            run_command(f"cd {dir} && git branch -M main")
+
+        run_command(f"cd {dir} && git add .")
+        commit_cmd = commit_command("Sync")
+        run_command(f"cd {dir} && {commit_cmd}")
+        remote_list = subprocess.getoutput(f"cd {dir} && git remote show")
+        if BACKUP_REMOTE_NAME in remote_list.split("\n"):
+            run_command(f"cd {dir} && git remote remove {BACKUP_REMOTE_NAME}")
+
+        branch_list = subprocess.getoutput(f"cd {dir} && git branch")
+        if f"* {BACKUP_BRANCH}" in branch_list.split("\n"):
+            run_command(f"cd {dir} && git branch -D {BACKUP_BRANCH}")
+
+        log_info(f"Encrypting {dir}...")
+        run_command(f"7z a -p{password} {dir}.7z {dir}/.git")
+
+    run_command("git add .")
+    run_command(commit_command("Sync"))
+    run_command("git push -u origin main")
+
+
+def add_to_gitignore(dir: str):
+    """Add the directory to the .gitignore"""
+    with open(".gitignore", "a+") as gitignore_file:
+        if dir not in gitignore_file.readlines():
+            gitignore_file.write(f"{dir}\n")
 
 
 def backup_notes():
     """Creates backup of the obsidian notes"""
+    log_info("Creating existing notes backup...")
     if os.path.exists(BACKUP_FOLDER):
         shutil.rmtree(BACKUP_FOLDER)
 
     os.makedirs(BACKUP_FOLDER)
-    for dir in get_obsidian_folder():
-        shutil.copytree(
-            dir,
-            f"{BACKUP_FOLDER}/{dir}",
-            ignore=shutil.ignore_patterns(f".git")
-        )
+    for dir in get_obsidian_folders():
+        backup_dir = f"{BACKUP_FOLDER}/{dir}"
+        shutil.copytree(dir, backup_dir)
+        if ".git" in listdir(dir):
+            run_command(f"cd {backup_dir} && git checkout -B {BACKUP_BRANCH}")
+            run_command(f"cd {backup_dir} && git add .")
+            commit_cmd = commit_command("Backup")
+            run_command(
+                f"cd {backup_dir} && {commit_cmd}",
+                ignore_error=True,
+            )
+        log_success("Notes successfully backed up!")
 
 
-def get_obsidian_folder():
+def get_obsidian_folders():
     """Retrieves the obsidian vaults' name in the current directories"""
-    return filter(
-        lambda dir: isdir(dir) and dir != ".git" and ".obsidian" in listdir(dir),
-        listdir(),
-    )
+    return filter(is_obsidian_vault, listdir())
+
+
+def is_obsidian_vault(dir: str):
+    """
+    Returns True if the provided path points to an Obsidian vaults, False
+    otherwise.
+    """
+    return isdir(dir) and dir != ".git" and ".obsidian" in listdir(dir)
+
+
+def contains_git_dir(dir: str):
+    """
+    Returns True if the directory contain a .git folder, False otherwise.
+    """
+    return isdir(dir) and ".git" in listdir(dir)
+
+
+def commit_command(msg: str) -> str:
+    """Returns the git commit command with th provided message."""
+    return f"git diff-index --quiet HEAD || git commit -m '{msg}'"
 
 
 def main():
@@ -146,5 +261,4 @@ def main():
 
 
 if __name__ == "__main__":
-    backup_notes()
-    # main()
+    main()
